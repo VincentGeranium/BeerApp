afeb277056de29f7c5865e32052eb2570c01d7e1
김광준 <kwangjun3952@gmail.com>
2020-06-27 20:27:57 +0900

    init

---
+++ BeerApp/BeerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerApp
+//
+//  Created by 김광준 on 2020/06/27.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
