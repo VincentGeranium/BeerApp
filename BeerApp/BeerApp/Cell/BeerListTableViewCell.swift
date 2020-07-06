52e6ed5a3d661d98dddf4f039e9b4eacf0983a31
김광준 <kwangjun3952@gmail.com>
2020-07-07 01:43:51 +0900

    맥주 이름, 맥주 설명 레이블 생성

--- BeerApp/BeerApp/Cell/BeerListTableViewCell.swift
+++ BeerApp/BeerApp/Cell/BeerListTableViewCell.swift
@@ -23,8 +23,43 @@ class BeerListTableViewCell: UITableViewCell {
         var beerIdLabel: UILabel = UILabel()
         beerIdLabel.backgroundColor = .systemPink
         beerIdLabel.textAlignment = .left
+        beerIdLabel.textColor = UIColor.systemGray
         beerIdLabel.text = "1"
         return beerIdLabel
+    }()
+    
+    private let beerNameLabel: UILabel = {
+        var beerNameLabel: UILabel = UILabel()
+        beerNameLabel.backgroundColor = .systemPurple
+        beerNameLabel.textAlignment = .left
+        beerNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        beerNameLabel.text = "Buzz"
+        return beerNameLabel
+    }()
+    
+    private let beerDescriptionLabel: UILabel = {
+        var beerDescriptionLabel: UILabel = UILabel()
+        beerDescriptionLabel.backgroundColor = .systemBlue
+//        beerDescriptionLabel.textAlignment = .left
+        beerDescriptionLabel.lineBreakMode = .byWordWrapping
+        beerDescriptionLabel.numberOfLines = 0
+        beerDescriptionLabel.textColor = UIColor.systemGray
+        beerDescriptionLabel.text = """
+        I'm on my second guess Or maybe my second best
+        What do I have to do?
+        To know that I'm getting through
+        I don't have much to say Just hoping you'd come my way What do
+        I have to do? To come and get close to you Come get close to you Baby
+        I won't slow down and
+        I won't say when Tell me why
+        I be comin' round again
+        I'm ready to go again Baby
+        I won't slow down and
+        I won't say no Just one look and
+        I think it starts to show
+        I think that I already know
+        """
+        return beerDescriptionLabel
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -32,6 +67,8 @@ class BeerListTableViewCell: UITableViewCell {
         
         setUpBeerImageView()
         setUpBeerIdLabel()
+        setUpBeerNameLabel()
+        setUpBeerDescriptionLabel()
     }
     
     required init?(coder: NSCoder) {
@@ -79,6 +116,34 @@ class BeerListTableViewCell: UITableViewCell {
             beerIdLabel.leadingAnchor.constraint(equalTo: beerImageView.trailingAnchor, constant: 30),
             beerIdLabel.trailingAnchor.constraint(equalTo: guide().trailingAnchor),
             beerIdLabel.heightAnchor.constraint(equalTo: guide().heightAnchor, multiplier: 0.2),
+        ])
+    }
+    
+    private func setUpBeerNameLabel() {
+        
+        beerNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(beerNameLabel)
+        
+        NSLayoutConstraint.activate([
+            beerNameLabel.topAnchor.constraint(equalTo: beerIdLabel.bottomAnchor),
+            beerNameLabel.leadingAnchor.constraint(equalTo: beerImageView.trailingAnchor, constant: 30),
+            beerNameLabel.trailingAnchor.constraint(equalTo: guide().trailingAnchor),
+            beerNameLabel.heightAnchor.constraint(equalTo: guide().heightAnchor, multiplier: 0.2)
+        ])
+    }
+    
+    private func setUpBeerDescriptionLabel() {
+        
+        beerDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(beerDescriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            beerDescriptionLabel.topAnchor.constraint(equalTo: beerNameLabel.bottomAnchor),
+            beerDescriptionLabel.leadingAnchor.constraint(equalTo: beerImageView.trailingAnchor, constant: 30),
+            beerDescriptionLabel.trailingAnchor.constraint(equalTo: guide().trailingAnchor),
+            beerDescriptionLabel.bottomAnchor.constraint(equalTo: guide().bottomAnchor),
         ])
     }
     
