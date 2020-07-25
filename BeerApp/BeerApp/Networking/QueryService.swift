9a7b36ccd267099fc4eacb6537c98021d1a425ff
김광준 <kwangjun3952@gmail.com>
2020-07-26 03:00:17 +0900

    URLComponent 작업 중

--- BeerApp/BeerApp/Networking/QueryService.swift
+++ BeerApp/BeerApp/Networking/QueryService.swift
@@ -9,8 +9,18 @@
 import Foundation
 
 class QueryService {
+    let defaultURLSession = URLSession(configuration: .default)
+    var dataTask: URLSessionDataTask?
+    
+    
     typealias queryResult = ([Beers]?, String) -> ()
     func getSearchResult(searchId: String, completion: @escaping queryResult) {
-        
+        /// if var 인 이유  urlComponents.query = "?ids=\(searchId)"
+        /// 정확하게는 모르겠음. 아마 query로 인해 검색하면 계속해서 url이 바뀌기 때문인가??
+        if var urlComponents = URLComponents(string: "https://api.punkapi.com/v2/beers") {
+            urlComponents.query = "?ids=\(searchId)"
+            
+            guard let url = urlComponents.url else { return }
+        }
     }
 }
